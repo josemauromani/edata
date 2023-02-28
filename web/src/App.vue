f90e68730d8d1605ef553062c212a2de9496b5b2
Jose Mauro <jose.mani@oficinacerta.app>
2023-02-27 23:06:25 -0300

    updates

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">EData</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/users" class="nav-link">Listar Usuarios</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Cadastrar</router-link>
+        </li>
+      </div>
+    </nav>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default defineComponent({
+  name: "App",
+});
+</script>
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,20 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    alias: "/users",
+    name: "users",
+    component: () => import("../components/user/UserList.vue"),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: "/users/:id",
+    name: "user-details",
+    component: () => import("../components/user/UserDetails.vue"),
+  },
+  {
+    path: "/add",
+    name: "add",
+    component: () => import("../components/user/AddUser.vue"),
+  },
 ]
 
 const router = createRouter({
